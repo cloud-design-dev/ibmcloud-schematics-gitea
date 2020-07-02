d8002b67b37eb3f86c2e2306a4762558f895c504
greyhoundforty <ryantiffany@fastmail.com>
2020-07-02 16:09:08 -0500

    Fixed output file
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "root_db_password" {
-  value = random_id.rootpass.hex
-}
-
-output "db_password" {
-  value = random_id.dbpass.hex
-}
-
 output "instance_ip" {
   value = ibm_compute_vm_instance.gitea_node.ipv4_address
 }
