9f21b09150d362e55cc949710553bba49a5cb70d
greyhoundforty <ryantiffany@fastmail.com>
2020-07-02 14:56:58 -0500

    Fixed docker expose variables
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- installer.sh
+++ installer.sh
@@ -15,13 +15,16 @@ DEBIAN_FRONTEND=noninteractive add-apt-repository "deb [arch=amd64] https://down
 DEBIAN_FRONTEND=noninteractive apt -qqy update
 DEBIAN_FRONTEND=noninteractive apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install docker-ce docker-ce-cli containerd.io
 
+## Create internal docker network
+docker network create gitea
+
 ## Create data volumes for Gitea and Mysql
 docker volume create mysql
 docker volume create gitea 
 
 ## Create containers 
-docker create --name=db -e MYSQL_ROOT_PASSWORD=${db_root_password} -e MYSQL_USER=gitea -e MYSQL_PASSWORD=${db_password} -e MYSQL_DATABASE=gitea --expose 3306 -v mysql:/var/lib/mysql --network=gitea --restart=unless-stopped mysql:5.7
-docker create --name=gitea -e USER_UID=1000 -e USER_GID=1000 -e DB_TYPE=mysql -e DB_HOST=db:3306 -e DB_NAME=gitea -e DB_USER=gitea -e DB_PASSWD=${db_password} -v gitea:/data -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro --restart=unless-stopped --network=gitea --publish --expose 222 --expose 3000 gitea/gitea:latest
+docker create --name=db -e MYSQL_ROOT_PASSWORD=${db_root_password} -e MYSQL_USER=gitea -e MYSQL_PASSWORD=${db_password} -e MYSQL_DATABASE=gitea --expose=3306 -v mysql:/var/lib/mysql --network=gitea --restart=unless-stopped mysql:5.7
+docker create --name=gitea -e USER_UID=1000 -e USER_GID=1000 -e DB_TYPE=mysql -e DB_HOST=db:3306 -e DB_NAME=gitea -e DB_USER=gitea -e DB_PASSWD=${db_password} -v gitea:/data -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro --restart=unless-stopped --network=gitea --expose=222 --expose=3000 gitea/gitea:latest
 
 ## Update firewall rules
 ufw allow ssh
