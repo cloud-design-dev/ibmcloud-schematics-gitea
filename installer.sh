fd635fbd8dfc3694ca183ed125098d69bb8aaf19
greyhoundforty <ryantiffany@fastmail.com>
2020-07-02 06:16:09 -0500

    Removed docker-compose from installer
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- installer.sh
+++ installer.sh
@@ -11,18 +11,15 @@ DEBIAN_FRONTEND=noninteractive apt -qqy -o Dpkg::Options::='--force-confdef' -o 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 DEBIAN_FRONTEND=noninteractive add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-## Refresh package udpates and install Docker and Docker Compose
+## Refresh package udpates and install Docker
 DEBIAN_FRONTEND=noninteractive apt -qqy update
 DEBIAN_FRONTEND=noninteractive apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install docker-ce docker-ce-cli containerd.io
-curl -L https://github.com/docker/compose/releases/download/1.26.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-## Setup containers 
 
 ## Create data volumes for Gitea and Mysql
 docker volume create mysql
 docker volume create gitea 
 
+## Create containers 
 docker create --name=db -e MYSQL_ROOT_PASSWORD=${db_root_password} -e MYSQL_USER=gitea -e MYSQL_PASSWORD=${db_password} -e MYSQL_DATABASE=gitea --expose 3306 -v mysql:/var/lib/mysql --network=gitea --restart=unless-stopped mysql:5.7
 docker create --name=gitea -e USER_UID=1000 -e USER_GID=1000 -e DB_TYPE=mysql -e DB_HOST=db:3306 -e DB_NAME=gitea -e DB_USER=gitea -e DB_PASSWD=${db_password} -v gitea:/data -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro --restart=unless-stopped --network=gitea --publish --expose 222 --expose 3000 gitea/gitea:latest
 
@@ -32,6 +29,6 @@ ufw allow 3000
 ufw allow 222
 ufw --force enable
 
-## Start the containers 
+## Start containers 
 docker start db
 docker start gitea
